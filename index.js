78c75bb4d098b637b08a92c2628fbb69cfd016fd
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-01 15:38:19 +0600

    final server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const category = require ('./category.json')
 const details = require ('./categoryDetails.json')
+const hotels = require ('./hotels.json')
 
 const cors = require('cors')
 
@@ -33,6 +34,26 @@ app.get ('/category', (req, res) => {
 
 
   })
+
+app.get ('/hotels', (req, res) => { 
+
+  res.send (hotels)
+
+
+ 
+}) 
+
+app.get ('/hotels/:id', (req, res) => {
+
+  const id = req.params.id;
+  const hotel = hotels.filter (c => c.id == id)
+
+  res.send (hotel)
+
+
+ })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
